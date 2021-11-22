30cee55e03f3b3eba97143ed2d26b2efcb959da5
Marek Pierzchała <markph1990@gmail.com>
2021-11-22 12:40:02 +0100

    add styling for jokes component

--- src/components/JokesList/view.tsx
+++ src/components/JokesList/view.tsx
@@ -1,13 +1,17 @@
 import { Joke } from "../../types";
 
+import "./style.css";
+
 interface OwnProps {
   jokes: Joke[];
 }
 
 const JokesList = ({ jokes }: OwnProps) => (
-  <ul>
-    {jokes.map(({ text }) => (
-      <li>{text}</li>
+  <ul className="jokes">
+    {jokes.map(({ text, id }) => (
+      <li key={id} className="joke">
+        {text}
+      </li>
     ))}
   </ul>
 );
--- src/pages/Jokes/view.tsx
+++ src/pages/Jokes/view.tsx
@@ -1,11 +1,22 @@
-import { Joke } from "../../types";
-
 import JokesList from "../../components/JokesList/view";
 
 import "./style.css";
 
 // temporary, till we get real data from api
-const jokesMock = [{ text: "joke 1" }, { text: "joke 2" }, { text: "joke 3" }];
+const jokesMock = [
+  {
+    id: "1",
+    text: "Molestie ac, feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa, eget egestas purus viverra accumsan!",
+  },
+  {
+    id: "2",
+    text: "Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc vel risus.",
+  },
+  {
+    id: "3",
+    text: "Vulputate enim nulla aliquet porttitor lacus, luctus accumsan tortor posuere.",
+  },
+];
 
 const Jokes = () => (
   <div className="page">
